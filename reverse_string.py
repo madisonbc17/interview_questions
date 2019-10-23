e048e6306bad5292e38c80deb226bfb94a850ef5
Madison Carr <madisonbc17@gmail.com>
2019-10-23 16:14:00 -0600

    Reverse string interview problem

---
+++ reverse_string.py
@@ -0,0 +1,49 @@
+import re
+
+def reverse_sentence_complicated(sentence):
+    words = []
+    word = ""
+    final_sentence = ""
+    sentence_len = len(sentence)
+
+    #find the words in the sentence
+    for i in sentence:
+        if i.isspace() != True:
+            word += i
+        else:
+            words.append(word)
+            word = ""
+
+        #set word to the list of words        
+    words.append(word)
+
+    #format the sentence, but reversed
+    for j in words:
+        if j == words[0]:
+            final_sentence = j
+        else:
+            final_sentence = j + " " + final_sentence
+
+    return final_sentence
+
+def reverse_sentence_simple(sentence):
+    #separate the words in the sentence
+    words = sentence.split(" ")
+
+    #format the sentence, but reversed
+    for i in words:
+        if words[0] == i:
+            final_sentence = i
+        else:
+            final_sentence = i + " " + final_sentence
+
+    return final_sentence
+
+sentence = "Go BYU Cougars"
+
+reversed_sentence = reverse_sentence_complicated(sentence)
+second_reversed_sentence = reverse_sentence_simple(sentence)
+
+print(sentence)
+print(reversed_sentence)
+print(second_reversed_sentence)
