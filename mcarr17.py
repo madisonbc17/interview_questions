996e260f6d707f6e25cb2c55facec42deec4b258
Madison Carr <madisonbc17@gmail.com>
2019-12-27 12:57:34 -0700

    Adding a message in the CLI

--- mcarr17.py
+++ mcarr17.py
@@ -1,6 +1,7 @@
 current_assignments = ["source_code_control","Teamwork: personality styles"]
 finished_assignments = []
 
+print("*****Assignments left to do in this semester*****")
 for busy_work in current_assignments:
     current_assignments.remove(busy_work)
     finished_assignments.append(busy_work)
@@ -9,6 +10,6 @@ print("Needing completion:")
 print(current_assignments)
 print("Finished assignments:")
 print(finished_assignments)
-
+ 
 assignments_left = len(current_assignments)
 print(str(assignments_left) + " assignments left in the semester")
