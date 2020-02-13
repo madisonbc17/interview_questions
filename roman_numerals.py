8ebfa5f033e772dcbb481565d80db98f9bf2f5ef
madisonbc17 <madisonbc17@gmail.com>
2020-02-12 23:07:32 -0700

    A new interview question

---
+++ roman_numerals.py
@@ -0,0 +1,3 @@
+#for this program, the input is a Roman numeral (string) and the output is the equivalent number (int).
+#the only possibilities for Roman numeral characters are I, V, X and L.
+#there is a max string length of four for each Roman numeral input- so the highest value would be LXXX or 80
